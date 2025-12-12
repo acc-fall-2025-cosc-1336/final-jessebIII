78acb5f97ec0cb6263a7fa6b6848553e46f4f222
Jesse Borunda <jesse.borunda@g.austincc.edu>
2025-12-12 04:53:34 +0000

    main py written

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,4 +1,12 @@
 #add import
-from question_a import print_multiplication_table
+from question_a import create_multiplication_table, display_multiplication_table
 
-print_multiplication_table()
+continue_program = True
+while continue_program:
+    table = create_multiplication_table()
+    display_multiplication_table(table)
+    
+    user_choice = input("\nDo you want to continue? (yes/no): ").lower()
+    if user_choice != 'yes':
+        continue_program = False
+        print("Goodbye!")
