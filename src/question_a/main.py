1e9fb987a566d018fd70e5f65d35fb3db71f55c3
Jesse Borunda <jesse.borunda@g.austincc.edu>
2025-12-12 04:46:40 +0000

    create a multiplication table with these characters

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,4 @@
 #add import
+from question_a import print_multiplication_table
+
+print_multiplication_table()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,16 @@
 #write functions here, don't add input('') statements here!
+def multiplication_table(rows=5, cols=10):
+    """Create a multiplication table using lists"""
+    table = [[i * j for j in range(1, cols + 1)] for i in range(1, rows + 1)]
+    return table
+
+
+def print_multiplication_table(rows=5, cols=10):
+    """Print a formatted multiplication table"""
+    table = multiplication_table(rows, cols)
+    for row in table:
+        print(' '.join(f'{num:>2}' for num in row))
+
+
 def test_config():
     return True
