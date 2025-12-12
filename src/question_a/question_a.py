db224b1112ed33fd738af8e103b797246ab94625
Jesse Borunda <jesse.borunda@g.austincc.edu>
2025-12-12 04:49:51 +0000

    loop written

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,6 +1,12 @@
 #write functions here, don't add input('') statements here!
 def create_multiplication_table():
-    return []
+    table = []
+    for i in range(1, 6):
+        row = []
+        for j in range(1, 11):
+            row.append(i * j)
+        table.append(row)
+    return table
 
 
 def test_config():
