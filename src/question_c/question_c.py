142e8b2b3d46e5459e46616afee7cc5e79005280
Jesse Borunda <jesse.borunda@g.austincc.edu>
2025-12-12 05:33:00 +0000

    test case for question c

--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,7 @@
 #write functions here, don't add input('') statements here!
+def get_most_likely_ancestor_consensus(dna_string1, dna_string2):
+    positions = []
+    for i in range(len(dna_string1) - len(dna_string2) + 1):
+        if dna_string1[i:i + len(dna_string2)] == dna_string2:
+            positions.append(i + 1)  # Add 1 to convert to 1-based indexing
+    return tuple(positions)
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -1,12 +1,24 @@
 #write function tests here, don't add input('') statements here!
 import unittest
+import sys
+sys.path.insert(0, '/workspaces/final-jessebIII')
 
 #follow this example to add questions b, c, and d for testing including their functions
 from src.question_a.question_a import test_config
+from src.question_c.question_c import get_most_likely_ancestor_consensus
 
 class Test_Config(unittest.TestCase):
 
     def test_question_a_config(self):
         self.assertEqual(True, test_config())
+
+
+class Test_Question_C(unittest.TestCase):
+
+    def test_get_most_likely_ancestor_consensus(self):
+        pos1, pos2, pos3 = get_most_likely_ancestor_consensus("GATATATGCATATACTT", "ATAT")
+        self.assertEqual(2, pos1)
+        self.assertEqual(4, pos2)
+        self.assertEqual(10, pos3)
 
 
